15ab37ed0a456ff6707c04b1bdd7cfcdabc06d89
Kim A. Betti <kim.betti@acando.no>
2015-07-21 23:32:01 +0200

    Simlified broadcast api

--- src/main/resources/webroot/static/js/app/application.js
+++ src/main/resources/webroot/static/js/app/application.js
@@ -16,19 +16,11 @@ window.pubsub = (function() {
     // Dead simple pubsub event-bus with reactive
     // capabilities provided by Bacon.js
     return {
-        broadcast: function() {
-            if (arguments.length === 1) {
-                bus.push(arguments[0]);
-            }
-            else if (arguments.length === 2) {
-                bus.push({
-                    namespace: arguments[0],
-                    data: arguments[1]
-                });
-            }
-            else {
-                throw "Don't know what to do with this"
-            }
+        broadcast: function(namespace, data) {
+            bus.push({
+                namespace: namespace,
+                data: data
+            });
         },
 
         takeOne: function(type) {
@@ -47,7 +39,7 @@ pubsub.stream("*").log("Bus activity");
 /**
  * Define some useful global streams and properties
  */
-var globalBacon = (function() {
+var wsBacon = (function() {
     var websocketConnecting = pubsub.stream("websocket-connecting");
     var websocketConnectionFailed = pubsub.stream("websocket-connection-failed");
     var websocketOpened = pubsub.stream("websocket-opened");
@@ -87,40 +79,27 @@ var globalBacon = (function() {
 
             socket.onmessage = function (event) {
                 var json = JSON.parse(event.data);
-
-                pubsub.broadcast({
-                    namespace: json.namespace,
-                    data: json.payload
-                });
+                pubsub.broadcast(json.namespace, json.payload);
             };
 
             socket.onerror = function(event) {
-                pubsub.broadcast({
-                    namespace: "websocket-error",
-                    data: event
-                });
+                pubsub.broadcast("websocket-error", event);
             };
 
             socket.onopen = function (event) {
-                pubsub.broadcast({
-                    namespace: "websocket-opened",
-                    data: event
-                });
+                pubsub.broadcast("websocket-opened", event);
             };
 
             socket.onclose = function (event) {
-                pubsub.broadcast({
-                    namespace: "websocket-closed",
-                    data: event
-                });
+                pubsub.broadcast("websocket-closed", event);
             };
         };
 
 
         Bacon.once()
             .merge(Bacon.interval(5000))
-            .filter(globalBacon.isWebsocketOpen.not())
-            .filter(globalBacon.isWebsocketConnecting.not())
+            .filter(wsBacon.isWebsocketOpen.not())
+            .filter(wsBacon.isWebsocketConnecting.not())
             .assign(createSocket, "ws://localhost:9000");
     }
     else {
